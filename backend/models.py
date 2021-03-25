c05932252f88996e5ffbc672c4e0acad2f6f14da
EdmundHusserl <japemjean@gmail.com>
2021-03-25 01:00:28 -0400

    [main] docker-compose file to deploy psql container

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,11 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_name = "trivia_test"
+database_path = "postgresql://{}:{}@{}/{}".format("jorgepl",
+                                                  "admin",
+                                                  'localhost:5677', 
+                                                  database_name)
 
 db = SQLAlchemy()
 
