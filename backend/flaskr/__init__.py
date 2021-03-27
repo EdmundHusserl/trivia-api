c6508009c5937bfb53418344a0dc721a2651e30e
EdmundHusserl <japemjean@gmail.com>
2021-03-26 22:12:09 -0400

    new route and pagination

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -71,6 +71,20 @@ def create_app(test_config=None):
             abort(405, METHOD_NOT_ALLOWED.format(request.method))
         else:
             abort(500)
+    
+    @app.route("/api/v1/categories/<int:id>")
+    def get_category_by_id(id: int) -> dict:
+        try:
+            cat: Category = Category.query.get(id)
+            if cat is None:
+                raise NotFound()
+            return cat.format()
+        except NotFound:
+            abort(404)
+        except MethodNotAllowed:
+            abort(405)
+        else: 
+            abort(500)
 
     """
     @TODO: 
@@ -82,6 +96,7 @@ def create_app(test_config=None):
     @app.route("/api/v1/questions", methods=["GET", "POST"])
     def get_post_questions() -> List[dict]:
         try:
+            import pdb; pdb.set_trace()
             if request.method == "GET":
                 questions: List[Question] = paginate(
                     results=Question.query.all(),
@@ -90,28 +105,30 @@ def create_app(test_config=None):
                 return jsonify([q.format() for q in questions])
             
             if request.method == "POST":
-                payload = QuestionPayload(request.form.get("question"),
-                                          request.form.get("answer"),
-                                          request.form.get("category"),
-                                          request.form.get("difficulty"))
+                req_body = request.get_json()
+                payload = QuestionPayload(req_body.get("question"),
+                                          req_body.get("answer"),
+                                          req_body.get("category"),
+                                          req_body.get("difficulty"))
                            
                 undefined_properties = payload.get_null_fields()
                 if undefined_properties:
-                    abort(422, 
-                          """Unprocessable payload as following fields are null: 
-                          {}.""".format(" - ".join(undefined_properties)))
-                
-                question = Question(question=payload.get("question"),
-                                    answer=payload.get("answer"),
-                                    category=payload.get("category"),
-                                    difficulty=payload.get("difficulty"))
+                    UnprocessableEntity(
+                        MSG_UNPROCESSABLE.format(params=" - ".join(undefined_properties))
+                    )
+                question = Question(question=payload.question,
+                                    answer=payload.answer,
+                                    category=payload.category,
+                                    difficulty=payload.difficulty)
                 question.insert()
                 return jsonify(question.format()), 201
 
         except MethodNotAllowed as e:
-            method = request.method
             logging.error(e.args)
-            abort(405, METHOD_NOT_ALLOWED.format(method))
+            abort(405, METHOD_NOT_ALLOWED.format(request.method))
+        except UnprocessableEntity as e:
+            logging.error(e.args)
+            abort(422, e.args[0] if len(e.args) else None)
         else:
             abort(500)
 
@@ -176,15 +193,17 @@ def create_app(test_config=None):
     def get_questions_using_search_term() -> List[dict]:
         try:
             search_term = request.form.get("search_term")
-            res_proxy: Iterable = Question.query.filter(
-                Question.question.ilike(f"%{search_term}%")
-            ) 
+            if search_term is None:
+                raise BadRequest()
             
-            questions = res_proxy.limit(5).all() if search_term is None else \
-                res_proxy.all()
-            
-            return jsonify([q.format() for q in questions])
+            res: List[Question] = paginate(
+                results=Question.query.filter(Question.question.ilike(f"%{search_term}%")).all(), 
+                start_at=request.args.get("page", 1, type=int)
+            )
+            return jsonify([q.format() for q in res])
         
+        except BadRequest:
+            abort(400, MSG_UNPROCESSABLE.format(params="search_term"))
         except MethodNotAllowed:
             abort(405, METHOD_NOT_ALLOWED.format(request.method))
         else:
@@ -205,9 +224,10 @@ def create_app(test_config=None):
     @app.route("/api/v1/categories/<int:cat_id>/questions", methods=["GET"])
     def get_questions_by_category(cat_id: int) -> List[dict]:
         try:
-            questions: List[Question] = Question.query.filter(
-                Question.category == cat_id
-            ).all()
+            questions: List[Question] = paginate(
+                results=Question.query.filter(Question.category == cat_id).all(),
+                start_at=request.args.get("page", 1, type=int)
+            )
             return jsonify([q.format() for q in questions])
         except NotFound:
             abort(404, f"Could not find a category with id={cat_id}")
@@ -233,8 +253,9 @@ def create_app(test_config=None):
     def get_questions_play():
         try:
             from random import randint
-            category = request.args.get("category")
-            previous_question = request.args.get("previous_question")
+            body = request.get_json()
+            category = body.get("category")
+            previous_question = body.get("previous_question")
             
             if category is None or previous_question is None:
                 raise UnprocessableEntity(MSG_UNPROCESSABLE.format(
@@ -247,7 +268,7 @@ def create_app(test_config=None):
             if not possible_questions:
                 raise NotFound(f"Could find any category with id={category}.")
             
-            random_q_excluding_previous = [el for el in filter(lambda x: x.id != previous_question,
+            random_q_excluding_previous = [el for el in filter(lambda x: x.id != int(previous_question),
                                                                possible_questions)]
             return random_q_excluding_previous[
                 randint(0, len(random_q_excluding_previous))].format()
@@ -277,7 +298,7 @@ def create_app(test_config=None):
             "status": 405,
             "success": False,
             "message": error.description if error.description else BASIC_MSG 
-        })
+        }), 405
     
     @app.errorhandler(500)
     def internal_server_error(error):
@@ -286,7 +307,7 @@ def create_app(test_config=None):
             "status": 500,
             "success": False,
             "message": error.description if error.description else BASIC_MSG 
-        })
+        }), 500
 
     @app.errorhandler(404)
     def not_found(error):
@@ -295,7 +316,7 @@ def create_app(test_config=None):
             "status": 404,
             "success": False,
             "message": error.description if error.description else BASIC_MSG 
-        })
+        }), 404
 
     @app.errorhandler(422)
     def unprocessable(error):
@@ -303,6 +324,6 @@ def create_app(test_config=None):
             "status": 422,
             "success": False,
             "message": "payload is unprocessable."
-        })
+        }), 422
     
     return app
